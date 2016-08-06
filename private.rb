75adba73fae52531b57efc47d2e8ae63c5b27767
anvil8789 <anvil8789@yahoo.co.jp>
2016-08-06 17:20:37 +0900

    privateに関する問題追加

---
+++ private.rb
@@ -0,0 +1,56 @@
+p "Q1"
+# 以下のコードを実行した結果を選べ
+# 1. hoge!!
+#    super!!
+#
+# 2. hoge!!
+#    実行時エラー「NoMethodError」
+#
+# 3. 実行時エラー「StandardError」
+#
+# 4. 実行時エラー「NameError」
+#
+class Foo
+    private
+    def hoge
+        p 'super!!'
+    end
+end
+
+class Bar < Foo
+    def hoge
+        p 'hoge!!'
+        super
+    end
+end
+
+Bar.new.hoge
+
+
+p "Q2"
+# 以下のコードを実行した結果を選べ
+# 1. hoge!!
+#    super!!
+#
+# 2. super!!
+#    super!!
+#
+# 3. 実行時エラー「NoMethodError」
+#
+# 4. 実行時エラー「NameError」
+#
+class High
+    private
+    def moge
+        p 'moge!!'
+    end
+end
+
+class Low < High
+    def hoge
+        p 'hoge!!'
+        moge
+    end
+end
+
+Low.new.hoge
